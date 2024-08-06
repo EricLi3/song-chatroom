1e45fd167f4db109929248eeba2497329e55108c
EricLi3 <ericlibeibei@gmail.com>
2024-08-06 16:13:22 -0400

    Added new grid mode and toggle to switch between views

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: 'https://music-discovery-api-3.onrender.com',
-    baseURL: 'http://3.138.181.50/',
+    baseURL: 'http://18.191.135.241',
 });
---
+++ src/components/greeting/Greeting.js
@@ -0,0 +1,8 @@
+const Greeting = () => {
+ 
+return (
+    <h1 style={{ textAlign: 'center' }}>Top 200 Songs</h1>
+  )
+}
+
+export default Greeting
